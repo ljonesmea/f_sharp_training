42afb95df94c262908cb69765a1656fb1655fdcf
Lisa Jones <122322604+ljonesmea@users.noreply.github.com>
2023-01-26 14:30:09 -0600

    Update Program.fs

--- Program.fs
+++ Program.fs
@@ -3,4 +3,7 @@ module Program =
     [<EntryPoint>]
     let main _ = 
         f_sharp_training.Test1()
+        f_sharp_training.Test2()
+        f_sharp_training.Test3()
+        f_sharp_training.Test4()
         0
